cfa55590bc068194c9c57806749f194975a5bb90
Ilyas <irazor147@gmail.com>
2023-06-05 10:30:46 +0200

    [ADD] mrp_subcontracting_dropshipping: port from v16

--- mrp_bom_attribute_match_duplicate_kit/__manifest__.py
+++ mrp_bom_attribute_match_duplicate_kit/__manifest__.py
@@ -1,17 +1,15 @@
-# Copyright 2022 Ooops. (https://www.ooops404.com) - Ashish Hirpara
+# Copyright 2023 Ooops. (https://www.ooops404.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "MRP Duplicate kit",
+    "name": "MRP BOM Attribute Match Duplicate Kit",
     "summary": "Duplicate product with MRP BOMs",
     "version": "14.0.1.0.0",
     "author": "Ooops, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
-    "contributors": ["Ashish"],
-    "maintainers": ["Ashish-Hirpara"],
     "category": "MRP",
-    "license": "LGPL-3",
-    "depends": ["mrp"],
+    "license": "AGPL-3",
+    "depends": ["mrp_bom_attribute_match"],
     "data": [
         "data/action_data.xml",
     ],
---
+++ mrp_bom_attribute_match_duplicate_kit/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* Ooops404 <https://ooops404.com>
+
+  * Ashish Hirpara <ashish.hirapara1995@gmail.com>
+  * Ilyas <irazor147@gmail.com>
